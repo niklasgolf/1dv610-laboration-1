d67505e5b643239350c9fec2fdbc32369b444c34
niklasgolf <niklas.herrloff@gmail.com>
2026-09-08 09:12:30 +0200

    Add visitor control bar design

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,29 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <header class="control-bar">
-    <p>Testar text i headern</p>
+    <div class="controls-left">
+      <input
+        id="name-input"
+        type="text"
+        placeholder="Skriv ditt namn"
+      />
+
+      <button id="start-button" type="button">
+        Start
+      </button>
+
+      <button id="stop-button" type="button">
+        Stop
+      </button>
+    </div>
+
+    <div class="status-area">
+      <span class="connection-status">● Connected</span>
+      <span class="visitor-count">0 visitors online</span>
+    </div>
   </header>
 
   <main class="visitor-area">
-    <p>Testar text i visitor-området</p>
+    <p>Visitor area</p>
   </main>
 `
